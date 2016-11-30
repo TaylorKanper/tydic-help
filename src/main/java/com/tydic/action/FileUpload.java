bf892630f2b61b8424e4517c0c04372b54af60e5
kangpeng <tony_kanper@hotmail.com>
2016-11-30 15:20:08 +0800

    修改服务器端的文件名为UUID算法生成

--- src/main/java/com/tydic/action/FileUpload.java
+++ src/main/java/com/tydic/action/FileUpload.java
@@ -1,7 +1,6 @@
 package com.tydic.action;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +9,8 @@ import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
 
 /**
  * Created by hp on 2016/11/29.
@@ -30,17 +31,17 @@ public class FileUpload {
                          @RequestParam("userfile") CommonsMultipartFile userfile) {
 
         String fileName = userfile.getOriginalFilename();
-
+        String serverPicName = UUID.randomUUID().toString()+"."+fileName.split("[.]")[1];
         ServletContext context = request.getSession().getServletContext();
 
-        String realPath = context.getRealPath( "/img/" + fileName);
+        String realPath = context.getRealPath("/img/" + serverPicName);
 
         try {
             userfile.getFileItem().write(new File(realPath));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return fileName;
+        return serverPicName;
     }
 
 }
